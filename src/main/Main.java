e243319a8230a1af01f01cb9d4d90426e2385ce4
Peter <peoplenamed@gmail.com>
2013-06-07 15:34:35 -0400

    Reading Writing of patterns works
    Fixed lots of bugs.
    Read - works
    Write - works * needs to re load file after write

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,14 +26,20 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -48,13 +54,16 @@ import com.sun.xml.internal.ws.util.StringUtils;
 public class Main {
 	public static JPanel ToolPanel = new JPanel();// This Panel holds the Top 4 panels
 	public static JPanel GridPanel = new JPanel();// Define the Panel that Holds
-												  // 256 Button Array
-
+	public static ArrayList <String> csvData = new ArrayList <String>();
+	public static  ArrayList<String> dataLine ;
+public static String[] PatternReader;
+	public static String[] header;					 
+	public static String[] row,row1,row2,row3,row4,row5,row6,row7,row8,row9,row10;
 	public static JPanel ToolPanelTopLeft = new JPanel();    // Top Left Panel
 	public static JPanel ToolPanelTopRight = new JPanel();   // Top Right Panel
 	public static JPanel ToolPanelBottomLeft = new JPanel(); // Bottom Left Panel
 	public static JPanel ToolPanelBottomRight = new JPanel();// Bottom Right Panel
-
+	public static String [][] board = new String [100][256];
 	public static JPanel FullPanel = new JPanel();    //This is the Panel the other panels will populate
 	public static JButton button[] = new JButton[256];// Define button array for
 													  // 256 buttons
@@ -77,7 +86,11 @@ public class Main {
 	public static JTextArea tracker; // used for debuging purposes. Will be removed in the end. Possibly replaced with another dialog
 	public static JPanel optPanel;
 	public static JFileChooser fileDialog;// File Chooser Dialog
+	public static JComboBox<String> ComboBox = new JComboBox();
+	
 
+	
+	
 	public static ActionListener ButtonArrayActionListener = new ActionListener() {
 		public void actionPerformed(ActionEvent ae1) {
 			Color newColor = colorChooser.getColor();
@@ -135,6 +148,38 @@ public class Main {
 
 		}
 	};
+	
+	
+	public static ActionListener ComboBoxActionListener = new ActionListener() {
+		public void actionPerformed(ActionEvent ae9) {
+			if (ComboBox.getItemCount()==0){
+				System.out.println("ERROR!!: There is nothing in the comboBox.");
+			}else{//
+				
+			
+			
+				if (csvData == null){//---
+					System.out.println("ERROR!!: csvData = null...." );//---
+				
+					
+				}else{//---
+					ColorChooserOutputText.setText(csvData.get(0));
+					
+					ColorChooserOutputText.setText("");//---
+					int ComboBoxSelectedIndex = ComboBox.getSelectedIndex();
+					ColorChooserOutputText.setText(csvData.get(ComboBoxSelectedIndex));
+					String[] line = csvData.get(ComboBoxSelectedIndex).split(",");
+						for (int RowNumber = 0; RowNumber<256; RowNumber++){//---
+			        		button[RowNumber].setBackground(Color.decode("0x"+line[RowNumber]));
+
+				       //---		        	
+					}
+				
+			        
+					}
+			}
+		}
+	};
 
 	public static void main(String[] args) {// Main (Run)
 		ColorChooserOutputText.setLineWrap(true);
@@ -159,6 +204,7 @@ public class Main {
 		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		ToolPanelTopLeft.add(scroll);
 		ToolPanel.add(ToolPanelTopLeft);
+		
 	}
 
 	public static void ToolPanelTopRight() {
@@ -203,6 +249,9 @@ public class Main {
 		optPanel.setLayout(new GridLayout(1, 2));
 
 		ToolPanelBottomRight.add(optPanel, BorderLayout.SOUTH);
+		ToolPanelBottomRight.add(ComboBox, BorderLayout.NORTH);
+		ComboBox.addActionListener(ComboBoxActionListener);
+		
 
 		JButton openButton = new JButton("Open Dialog");
 		openButton.setActionCommand("Open Dialog");
@@ -268,8 +317,30 @@ public class Main {
 		MaineWindow.setVisible(true);// Show Main Window
 	}
 
-	// Show a open file dialog box
-	public static void openDialog() {
+	
+	
+	
+	
+	
+	
+	
+	//////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	
+	/*
+	 public static String[][] openDialog()
+	  {
+	    FileInputStream inStream;
+	    InputStreamReader inFile;
+	    BufferedReader br;
+	    String line;
+	    int lineNum, tokNum, ii, jj;
+	    String [][] CSV, TempArray, TempArray2;
+
+	    lineNum = tokNum = ii = jj  = 0;
+	    TempArray = new String[50][50];
+
+	    ////////////////////////////
 		ColorChooserOutputText.setText("");	
 	int openChoice = fileDialog.showOpenDialog(ToolPanelBottomRight);
 		String[] strLine;
@@ -285,54 +356,163 @@ public class Main {
 			tracker.setText("");
 			tracker.append("\nThe file selected is " + openFile.getName());
 			tracker.append("\nThe file's path is " + openFile.getPath());
-try {
-				File f = fileDialog.getSelectedFile();
+   ////////////////////////////////////////////////
+	    
+	    try
+	    {
+	    	
+			BufferedReader CSVFile = new BufferedReader(new FileReader(openFile));//load file in to reader 
+			String dataRow = CSVFile.readLine(); // Read the first line of data.
+	    	
+	    	
+	    	URL url = new URL(openFile.getAbsolutePath());
+	    	
+	        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+	        System.out.println("Please enter the file path of the CSV");
+	        String fileName = in.readLine();
+	        inStream = new FileInputStream(fileName);
+	        inFile = new InputStreamReader(inStream);
+	        br = new BufferedReader(inFile);
+	        StringTokenizer tok,tok2;
 
-			
-				CSVReader reader = new CSVReader(new FileReader(f));
-			
-				while ((strLine = reader.readNext()) != null) {
 
-					for(int i=1; i<256; i++){
-//This code is for using a formatted file with characters other than HEX CSV (hex,hex,hex) vs (#hex,#hex,#hex)
-		//			ColorstrLine =	stripChars(strLine[i], "#");
-		//			String colr2 = ColorstrLine.trim();
-		//			ColorChooserOutputText.append(colr2 + ",");
-		//		 	button[i].setBackground(Color.decode("0x"+colr2));
-						
-						ColorChooserOutputText.append(strLine[i] + ",");
-					 	
-						button[i].setBackground(Color.decode("0x"+strLine[i]));
-					
-					}
+	        lineNum = 0;
+	            line = br.readLine();
+	            tokNum = 0;
+	            tok = new StringTokenizer(line, ",");
 
-				 
-				} 
-				reader.close();
-			} catch (Exception e) {
-				System.err.println("Idiot, Error: " + e.getMessage());
-			}
-				
-		}
-	}
+	            while( tok.hasMoreTokens())
+	            {
+	                TempArray[tokNum][0] = tok.nextToken();
+	                tokNum++;
+	            }
+	            tokNum = 0;
+	            lineNum++;
 
-	
-	/*Useful but not needed at this time
-	
-	
-	public static String stripChars(String input, String strip) {
-	    StringBuilder result = new StringBuilder();
-	    for (char c : input.toCharArray()) {
-	        if (strip.indexOf(c) == -1) {
-	            result.append(c);
-	        }
+	            while( line != null)
+	            {
+	                    line = br.readLine();
+	                    if (line != null)
+	                    {
+	                        tokNum = 0;
+	                        tok2 = new StringTokenizer(line, ",");
+
+	                        while(tok2.hasMoreTokens())
+	                        {
+	                            TempArray[tokNum][lineNum] = tok2.nextToken();
+	                            tokNum++;
+	                        }
+	                    }
+	                    lineNum++;
+	            }
+	    }    
+	    catch(IOException e)
+	    {
+	        System.out.println("Error file  may not be accessible, check the path and try again");
 	    }
-	    return result.toString();
+
+	    CSV = new String[tokNum][lineNum-1];
+	    for (ii=0; ii<tokNum ;ii++)
+	    {
+	        System.arraycopy(TempArray[ii],0,CSV[ii],0,lineNum-1);
+	    }
+	    return CSV;
+	}    
+	///////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// Show a open file dialog box
+	public static void openDialog() {
+		
+		
+		
+		
+		ColorChooserOutputText.setText("");	// ---
+		int openChoice = fileDialog.showOpenDialog(ToolPanelBottomRight);// ---
+		logChoice(openChoice, "Open Dialog");// ---
+		if (openChoice == JFileChooser.APPROVE_OPTION) {// ---
+			// Put open file code in here
+			File openFile = fileDialog.getSelectedFile();// ---
+			tracker.setText("");// ---
+			tracker.append("\nThe file selected is " + openFile.getName());// ---
+			tracker.append("\nThe file's path is " + openFile.getPath());// ---
+		//	readCSVFile(openFile);//---
+		//}//}
+		
+	//	public static void readCSVFile(File openFile){//---
+			ComboBox.removeAllItems();;
+			try {//---
+				 
+				 
+			 FileReader fileReader = new FileReader(openFile);//---
+			       BufferedReader bufferedReader = new BufferedReader(fileReader);//---
+			     
+			     CSVReader csvReader = new  CSVReader(new FileReader (openFile));
+					String line = null;
+						while ((line = bufferedReader.readLine()) != null) {
+						
+							 String[] splitted = line.split("H");
+				 
+							 dataLine = new ArrayList<String>(splitted.length);
+
+					            for (String data : splitted)
+					                dataLine.add(data);
+					            csvData.addAll(dataLine);
+					            // System.out.println(dataLine);
+					            //System.out.println(csvData);
+				            }
+/*Print ALL Lines
+			            String [] number = new String [csvData.size()];
+
+					        for(int z = 0; z < csvData.size(); z++)
+					        {
+					            number[z] = csvData.get(z);
+					          System.out.println(number[z]);
+					            System.out.println("1***");
+					        }
+Print ALL Lines*/
+			            
+				        for(int z = 0; z < csvData.size(); z++)
+				        {
+				        	 ComboBox.addItem("Pattern# "+z);
+				        }
+					        ColorChooserOutputText.setText(csvData.get(0));
+					      					        
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		
+			
 	}
-		*/
+		
+		
 	
-	
-	// Show a save file dialog box
+	// Show a save file dialog box and save colors
 	private static void saveDialog() {
 		CreateCodePlease();
 
@@ -352,8 +532,9 @@ try {
 
 				BufferedWriter writer = null;
 				writer = new BufferedWriter(new FileWriter(
-						saveFile.getAbsolutePath()+ ".kola"));
-				writer.write(ReturnSaveVal);
+						saveFile.getAbsolutePath(), true));
+				//saveFile.getAbsolutePath()+ ".kola", true));
+				writer.write(ReturnSaveVal+ "H");
 				writer.close();
 
 				infoBoxCorrect();
